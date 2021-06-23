c8021065655c8d1fad2779547c4c3b790f517328
Jihye <kimji9663@nate.com>
2021-06-23 18:47:22 +0900

    javascript 배열의 map() 함수 알기

---
+++ my-app/src/IterationSample.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+/** map 함수 알기
+ * 기존 배열로 새로운 배열을 만드는 역할
+
+ ** ES5 문법
+    var numbers = [1, 2, 3, 4, 5];
+    var result = numbers.map(function(num){
+    return num * num;
+    console.log(result);
+    });
+
+ ** ES6 문법
+    const numbers = [1, 2, 3, 4, 5];
+    const result = number.map(num => num * num);
+    console.log(result);
+ */
+
+const IterationSample = () => {
+    return (
+        <ul>
+            <li>눈사람</li>
+            <li>얼음</li>
+            <li>눈</li>
+            <li>바람</li>
+        </ul>
+    )
+}
+
+export default IterationSample;
