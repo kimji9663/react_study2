330d1691cb72dc9a2b31dfe397b26c26fa5d578e
Jihye <kimji9663@nate.com>
2021-07-23 11:53:57 +0900

    뒷정리(cleanup) 하기

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Info from './Info';
 
 const App = () => {
-    return <Info />;
+    // 최초 마운트 시 안보이기 - false
+    const [visible, setVisible] = useState(false);
+    return (
+        <div>
+            <button
+                onClick={() => {
+                    setVisible(!visible);
+                }}
+            >
+                {visible ? '숨기기' : '보이기'}  {/* 조건 ? 'true' : 'fase' */}
+            </button>
+            <hr />
+            {visible && <Info />}
+        </div>
+    );
 };
 
 export default App;
--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -13,7 +13,7 @@ const Info = () => {
     //     });
     // });
 
-    // 마운트 될 때마다 실행
+    // 마운트 될 때마다 실행(빈 배열)
     // useEffect(() => {
     //     console.log('마운트 될 때만 실행됩니다.');
     //     console.log({
@@ -22,10 +22,25 @@ const Info = () => {
     //     }, []);
     // });
 
-    // 업데이트 될 때마다 실행
+    // 업데이트 될 때마다 실행(배열안에 검사값)
+    // useEffect(() => {
+    //     console.log(name);
+    // },[name]);
+
+    // 기본적으로는 랜더링 직후 실행, 
+    // 두번째 파라미터 배열[]에 무엇을 넣느냐에 따라 실행조건이 달라진다.
+
+    // 언마운트 전이나 업데이트 전에 어떤 작업을 수행하고 싶으면 cleanup 함수를 반환해줘야 한다.
     useEffect(() => {
+        console.log('effect');
         console.log(name);
-    },[name]);
+
+        // cleanup 함수
+        return () => {
+            console.log('cleanup');
+            console.log(name);
+        }
+    }, []); // 빈 배열 추가 (언마운트 될 때만 cleanup 함수 호출)
 
     const onChangeName = e => {
         setName(e.target.value);
