2f862986e0f7f1e383459ae66f69db1873d4fd94
Jihye <kimji9663@nate.com>
2021-07-23 10:55:34 +0900

    useEffect 업데이트 될 때만 실행

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -4,13 +4,28 @@ const Info = () => {
     const [name, setName] = useState('');
     const [nickname, setNickname] = useState('');
 
+    // 랜더링 될 때마다 실행
+    // useEffect(() => {
+    //     console.log('랜더링이 완료되었습니다.');
+    //     console.log({
+    //         name,
+    //         nickname
+    //     });
+    // });
+
+    // 마운트 될 때마다 실행
+    // useEffect(() => {
+    //     console.log('마운트 될 때만 실행됩니다.');
+    //     console.log({
+    //         name,
+    //         nickname
+    //     }, []);
+    // });
+
+    // 업데이트 될 때마다 실행
     useEffect(() => {
-        console.log('마운트 될 때만 실행됩니다.');
-        console.log({
-            name,
-            nickname
-        });
-    },[]);
+        console.log(name);
+    },[name]);
 
     const onChangeName = e => {
         setName(e.target.value);
