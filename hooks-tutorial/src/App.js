9b71c2bb0d19ed19637e70e9f6218e8c325f251f
Jihye <kimji9663@nate.com>
2021-07-26 11:48:41 +0900

    인풋 상태 관리하기

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,24 +1,8 @@
-import React, { useState } from 'react';
-import Counter from './Counter';
-//import Info from './Info';
+import React from 'react';
+import Info from './Info';
 
 const App = () => {
-    // 최초 마운트 시 안보이기 - false
-    //const [visible, setVisible] = useState(false);
-    return (
-        <div>
-            <Counter></Counter>
-            {/* <button
-                onClick={() => {
-                    setVisible(!visible);
-                }}
-            >
-                {visible ? '숨기기' : '보이기'}  {/* 조건 ? 'true' : 'fase' * /}
-            </button>
-            <hr />
-            {visible && <Info />} */}
-        </div>
-    );
+    return <Info />
 };
 
 export default App;
--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,60 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer(state, action){
+    return {
+        ...state,
+        [action.name]: action.value
+    };
+}
 
 const Info = () => {
-    const [name, setName] = useState('');
-    const [nickname, setNickname] = useState('');
-
-    // 랜더링 될 때마다 실행
-    // useEffect(() => {
-    //     console.log('랜더링이 완료되었습니다.');
-    //     console.log({
-    //         name,
-    //         nickname
-    //     });
-    // });
-
-    // 마운트 될 때마다 실행(빈 배열)
-    // useEffect(() => {
-    //     console.log('마운트 될 때만 실행됩니다.');
-    //     console.log({
-    //         name,
-    //         nickname
-    //     }, []);
-    // });
-
-    // 업데이트 될 때마다 실행(배열안에 검사값)
-    // useEffect(() => {
-    //     console.log(name);
-    // },[name]);
-
-    // 기본적으로는 랜더링 직후 실행, 
-    // 두번째 파라미터 배열[]에 무엇을 넣느냐에 따라 실행조건이 달라진다.
-
-    // 언마운트 전이나 업데이트 전에 어떤 작업을 수행하고 싶으면 cleanup 함수를 반환해줘야 한다.
-    useEffect(() => {
-        console.log('effect');
-        console.log(name);
-
-        // cleanup 함수
-        return () => {
-            console.log('cleanup');
-            console.log(name);
-        }
-    }, []); // 빈 배열 추가 (언마운트 될 때만 cleanup 함수 호출)
-
-    const onChangeName = e => {
-        setName(e.target.value);
-    };
-
-    const onChangeNickName = e => {
-        setNickname(e.target.value);
+    const [state, dispatch] = useReducer(reducer, {
+        name: '',
+        nickname: ''
+    });
+    const { name, nickname } = state;
+    const onChange = e => {
+        dispatch(e.target);
     };
 
     return (
         <div>
             <div>
-                <input value={name} onChange={onChangeName}></input>
-                <input value={nickname} onChange={onChangeNickName}></input>
+                <input name="name" value={name} onChange={onChange}></input>
+                <input name="nickname" value={nickname} onChange={onChange}></input>
             </div>
             <div>
                 <div>
