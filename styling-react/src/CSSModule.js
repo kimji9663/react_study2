15aa7f5f91d56decce67215d7c7f262dec863125
Jihye <kimji9663@nate.com>
2021-08-02 18:45:57 +0900

    classnames Sass와 함께 사용하기

--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classNames from 'classnames/bind';
-import styles from './CSSModule.module.css';
+import styles from './CSSModule.module.scss';
 
 // 미리 styles에서  클래스를 받아오도록 설정
 const cx = classNames.bind(styles);
