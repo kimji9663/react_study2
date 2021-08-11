8108a7c70626340da1b406de37d906e06fbff9f1
Jihye <kimji9663@nate.com>
2021-08-11 17:04:27 +0900

    수정 기능

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -26,26 +26,31 @@ const App = () => {
   const nextId = useRef(4);
 
   const onInsert = useCallback( text => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos(todos.concat(todo));
+    nextId.current += 1;
   }, [todos]);
 
-  const onRemove = useCallback(
-    id => {
-      setTodos(todos.filter(todo => todo.id !== id));
-    },
-    [todos],
-  )
+  const onRemove = useCallback( id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }, [todos]);
+
+  const onToggle = useCallback( id => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? {...todo, checked: !todo.checked} : todo,
+      ),
+    );
+  }, [todos]);
 
   return (
   <TodoTemplate>
       <TodoInsert onInsert={onInsert}></TodoInsert>
-      <TodoList todos={todos} onRemove={onRemove}></TodoList>
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}></TodoList>
   </TodoTemplate>
   );
 };
