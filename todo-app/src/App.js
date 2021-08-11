4070a73a72e04d1c0f6d4d4b01d0daa24bf0da05
Jihye <kimji9663@nate.com>
2021-08-11 15:40:55 +0900

    항목 추가 기능 구현하기

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useCallback } from "react";
+// useCallback: 컴포넌트가 최초 랜더링시에만 한번 함수를 만들고, 리렌더링 시에는 재사용 가능하다.
 import TodoInsert from "./components/TodoInsert";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoList from "./components/TodoList";
@@ -20,10 +21,23 @@ const App = () => {
       text: '일정 관리 앱 만들어보기',
       checked: false,
     },
-  ])
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback( text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+  }, [todos]);
+
   return (
   <TodoTemplate>
-      <TodoInsert></TodoInsert>
+      <TodoInsert onInsert={onInsert}></TodoInsert>
       <TodoList todos={todos}></TodoList>
   </TodoTemplate>
   );
