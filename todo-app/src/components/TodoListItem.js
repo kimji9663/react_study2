c6679669df79582120158a93de690917cd9e29c7
kimji9663 <rlawlgp9663@naver.com>
2021-09-01 00:07:58 +0900

    react-virtualized를 사용한 렌더링 최적화

--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -7,17 +7,19 @@ import {
 import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
     const { id, text, checked } = todo;
     return (
-        <div className="TodoListItem">
-            <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-                <div className="text">{text}</div>
-            </div>
-            <div className="remove" onClick={() => onRemove(id)}>
-            {/* 클릭 시 자신의 id를 onRemove함수에 전달한다. */}
-                <MdRemoveCircleOutline />
+        <div className="TodoListItem-virtualized" style={style}>
+            <div className="TodoListItem">
+                <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
+                    {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+                    <div className="text">{text}</div>
+                </div>
+                <div className="remove" onClick={() => onRemove(id)}>
+                {/* 클릭 시 자신의 id를 onRemove함수에 전달한다. */}
+                    <MdRemoveCircleOutline />
+                </div>
             </div>
         </div>
     )
