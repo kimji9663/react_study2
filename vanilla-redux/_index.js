25b7c2bd30e5bc96e2bf39d912e41995329c13e5
Jihye <kimji9663@nate.com>
2021-10-29 19:00:30 +0900

    리덕스 라이브러리 이해하기 - 리액트 없이 쓰는 리덕스 - 완성

--- vanilla-redux/_index.js
+++ vanilla-redux/_index.js
@@ -1,35 +1,30 @@
 import { createStore } from 'redux';
 
-// DOM 노드를 가리키는 값 선언
 const divToggle = document.querySelector('.toggle');
 const counter = document.querySelector('h1');
 const btnIncrease = document.querySelector('#increase');
 const btnDecrease = document.querySelector('#decrease');
 
-// 액션 이름 정의 (주로 문자열 형태, 고유한 이름으로 작성)
 const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
 const INCREASE = 'INCREASE';
 const DECREASE = 'DECREASE';
 
-// 액션 생성 함수
-const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
-const increase = difference => ({ type: INCREASE, difference });
-const decrease = () => ({ type: DECREASE });
+const toggleSwitch = () => ({ type : TOGGLE_SWITCH });
+const increase = difference => ({ type : INCREASE, difference });
+const decrease = () => ({ type : DECREASE });
 
-// 초기값 정의
 const initialState = {
     toggle: false,
     counter: 0
-};
+}
 
-// 리듀서 함수 정의 (파라미터로  state와 action값을 받아 옴)
 // state가 undefined일 때는 initialState를 기본값으로 사용
 function reducer(state = initialState, action) {
     // action.type에 따라 다른 작업을 처리함
     switch (action.type) {
         case TOGGLE_SWITCH:
             return {
-                ...state, //불변성 유지
+                ...state, //불변성 유지를 해 주어야 한다.
                 toggle: !state.toggle
             };
         case INCREASE:
@@ -47,25 +42,23 @@ function reducer(state = initialState, action) {
     }
 }
 
-const store = createStore(reducer);
+const store = createStore(reducer)
 
-// 랜더 함수 - 함수가 업데이트 될 때마다 호출
 const render = () => {
-    const state = store.getState();  // 현재 상태를 불러옵니다.
-
+    const state = store.getState(); // 현재 상태를 불러온다.
     // 토글 처리
     if (state.toggle) {
         divToggle.classList.add('active');
     } else {
-        divToggle.classList.remove('active');
+        divToggle.classList.remove('active')
     }
-
     // 카운터 처리
     counter.innerText = state.counter;
 };
 
 render();
-store.subscribe(render);
+
+store.subscribe(render)
 
 divToggle.onclick = () => {
     store.dispatch(toggleSwitch());
