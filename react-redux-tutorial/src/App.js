74e2779d474b0a439d417f67874568a045b19566
Jihye <kimji9663@nate.com>
2021-11-01 18:27:45 +0900

    컨테이너 컴포넌트 만들기 - CounterContainer 만들기

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import Counter from './components/Counter';
 import Todos from './components/Todos';
+import CounterContainer from './containers/CounterContainer';
 
 const App = () => {
   return (
     <div>
-      <Counter number={0} />
+      <CounterContainer />
       <hr />
       <Todos></Todos>
     </div>
-  )
-}
+  );
+};
 
 export default App;
