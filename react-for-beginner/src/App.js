17a6bbcb3d2871eac8b05911f2bdd407281fa610
Jihye <kimji9663@nate.com>
2021-11-10 18:44:17 +0900

    노마드코더 리액트기초 : To Do List 만들기

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,38 +1,27 @@
-import Button from "./Button";
 import {useState, useEffect} from 'react';
 
-function Hello() {
-  // 작성방법1
-  // function destroyFn() {
-  //   console.log("destroyed :(");
-  // }
-  // function createFn() {
-  //   console.log("created :)");
-  //   return destroyFn; // 나타날 함수 안에 사라질 함수를 리턴
-  // }
-  // useEffect(createFn, []); // useEffect(나타날 함수, [])
-
-  // 작성방법2 <---추천
-  useEffect(() => {
-    console.log("created :)");
-    return() => console.log("destroyed :(");
-  }, [])
-
-  return <h1>Hello</h1>;
-}
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => {
-    setShowing((prev) => !prev);
+  const [value, setValue] = useState("");
+  const onChange = (event) => {
+    setValue(event.target.value);
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(value);
+    if (value === ""){
+      return;
+    }
+    setValue("");
   }
 
   return (
     <div>
-      {/* hello가 보이게 하거나 안보이게 하거나 */}
-      {showing ? <Hello /> : null}
-      {/* 버튼명이 hide / show로 바뀜 */}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <form onSubmit={onSubmit}>
+      <input onChange={onChange} value={value} type="text" placeholder="Write your to do..." />
+      <button type="submit">Add to do</button>
+      </form>
     </div>
   );
 }
