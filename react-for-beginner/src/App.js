300d5ce1e754d6043e56b7a0fc01dc821c82cac1
Jihye <kimji9663@nate.com>
2021-11-11 14:52:09 +0900

    노마드코더 리액트기초 : To Do List 만들기

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -3,6 +3,8 @@ import {useState, useEffect} from 'react';
 
 function App() {
   const [value, setValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  
   const onChange = (event) => {
     setValue(event.target.value);
   }
@@ -13,15 +15,22 @@ function App() {
     if (value === ""){
       return;
     }
+    //react.js는 함수의 첫번째 arugment로 현재 state로 보낸다. (currentArray 최초값: [])
+    setTodos((currentArray) => [value, ...currentArray]);
     setValue("");
-  }
+  };
+  console.log(todos);
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-      <input onChange={onChange} value={value} type="text" placeholder="Write your to do..." />
-      <button type="submit">Add to do</button>
+        <input onChange={onChange} value={value} type="text" placeholder="Write your to do..." />
+        <button type="submit">Add to do</button>
       </form>
+      <ul>
+        {todos.map((item, index) => <li key={index}>{item}</li>)}
+        {/* map메서드의 첫번째, 두번째, 세번째 인자값 규칙: Array.map(value: any, index: number, array: any[]) */}
+      </ul>
     </div>
   );
 }
