20be1d359dac37078fdf13f1996efeb4937a172a
Jihye <kimji9663@nate.com>
2021-11-11 16:51:31 +0900

    노마드코더 리액트기초 : Coin Tracker

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,35 +1,29 @@
-import {useState, useEffect} from 'react';
+import {useEffect, useState} from 'react';
 
 
 function App() {
-  const [value, setValue] = useState("");
-  const [todos, setTodos] = useState([]);
-  
-  const onChange = (event) => {
-    setValue(event.target.value);
-  }
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
 
-  const onSubmit = (event) => {
-    event.preventDefault();
-    console.log(value);
-    if (value === ""){
-      return;
-    }
-    //react.js는 함수의 첫번째 arugment로 현재 state로 보낸다. (currentArray 최초값: [])
-    setTodos((currentArray) => [value, ...currentArray]);
-    setValue("");
-  };
-  console.log(todos);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((respoonse) => respoonse.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+    });
+  }, []);//한번만 실행.
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={value} type="text" placeholder="Write your to do..." />
-        <button type="submit">Add to do</button>
-      </form>
+      <h1>The Coins! ({coins.length})</h1>
+      
+        {loading ? <stron>Loading...</stron> : null}
+      
       <ul>
-        {todos.map((item, index) => <li key={index}>{item}</li>)}
-        {/* map메서드의 첫번째, 두번째, 세번째 인자값 규칙: Array.map(value: any, index: number, array: any[]) */}
+        {coins.map((coin) => (
+          <li>{coin.name}({coin.symbol}) : {Math.round(coin.quotes.USD.price * 100) / 100}USD</li>
+        ))}
       </ul>
     </div>
   );
