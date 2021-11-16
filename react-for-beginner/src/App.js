167ee971be7f5a6971d37d0f5d99e66ef871ee5a
Jihye <kimji9663@nate.com>
2021-11-16 10:10:24 +0900

    노마드코더 리액트기초 : Movie App

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -2,12 +2,17 @@ import {useEffect, useState} from 'react';
 
 
 function App() {
-  const [loading, setLoading] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
-    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-  }, [])
+    fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year")
+    .then((respoonse) => respoonse.json())
+    .then((json) => {
+      setMovies(json.data.movies);
+      setLoading(false);
+    });
+  }, []);//한번만 실행.
+  console.log(movies);
 
   return (
     <div>
