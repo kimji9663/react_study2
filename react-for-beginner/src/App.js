c36f7cbe1257b0ee5d061f1a3f4a0878162d3aed
Jihye <kimji9663@nate.com>
2021-11-16 10:22:52 +0900

    노마드코더 리액트기초 : Movie App(await-async)

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -4,14 +4,16 @@ import {useEffect, useState} from 'react';
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`)
+    //then 대신 await-async 사용
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
   useEffect(() => {
-    fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year")
-    .then((respoonse) => respoonse.json())
-    .then((json) => {
-      setMovies(json.data.movies);
-      setLoading(false);
-    });
-  }, []);//한번만 실행.
+    getMovies();
+  }, []);
   console.log(movies);
 
   return (
