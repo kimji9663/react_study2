3dc98f45e937a0bb827f0cbe77b30e5d423c1ea8
kimji9663 <rlawlgp9663@naver.com>
2021-11-21 23:58:38 +0900

    클래스형 컴포넌트로 바꾸기.1

--- class-component/src/App.js
+++ class-component/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-function Food({fav}) {
-  return <h1>I Love {fav}!</h1>
+function Food({name}) {
+  return <h1>I Love {name}!</h1>
 }
 
 const foodILike = [
@@ -25,12 +25,8 @@ const foodILike = [
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="삼겹살" />
-      <Food fav="라면" />
-      <Food fav="김치찌개" />
+    <div>
+      {foodILike.map(dish => <Food name={dish.name} />)}
     </div>
   );
 }
