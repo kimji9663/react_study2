5b4cce66f8f3f03cfc21ae4f197217765aaeca0f
Jihye <kimji9663@nate.com>
2021-11-22 14:02:25 +0900

    클래스형 컴포넌트로 바꾸기.2 (prop-types)

--- class-component/src/App.js
+++ class-component/src/App.js
@@ -1,32 +1,58 @@
 import React from 'react'
+import PropTypes from "prop-types";
 
-function Food({name}) {
-  return <h1>I Love {name}!</h1>
+function Food({name, picture, rating}) {
+  return (
+    <>
+      <h2>I Love {name}!</h2>
+      <h4>{rating} / 5.0</h4>
+      <img src={picture} alt={name} />
+    </>
+  )
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 const foodILike = [
   {
+  id: 1,
   name: "Kimchi",
-  image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+  image: "https://w.namu.la/s/a85e178b2233fa1e3461ae51dba6757e8cc87b215ea326660c85904f61daf0dcd84f8b8733cee71ca34fa743822d5aa1650766f6b2a90118edc5e76974e8305263694a4c2358508602ad7694e2cd022389545b2812f6e86bf29e05e172b53405",
+  rating: 5
   },
   {
+  id: 2,
   name: "bulgogi",
-  image: "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM"
+  image: "https://recipe1.ezmember.co.kr/cache/recipe/2019/03/03/11baafbe81803965b17c3ab42a5992cb1.jpg",
+  rating: 4
   },
   {
+  id: 3,
   name: "kimbap",
-  image: "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM"
+  image: "https://static.wtable.co.kr/image-resize/production/service/recipe/689/4x3/edd1e01f-4502-453b-8b61-688941aefb76.jpg",
+  rating: 3
   },
   {
+  id: 4,
   name: "samgyetang",
-  image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+  image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+  rating: 2
   }
 ];
+
+function renderFood(dish){
+  return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+}
 
 function App() {
   return (
     <div>
-      {foodILike.map(dish => <Food name={dish.name} />)}
+      {console.log(foodILike.map(renderFood))}
+      {foodILike.map(renderFood)}
     </div>
   );
 }
