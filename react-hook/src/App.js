c663747b2c51e88eb3d1db2f747f602f111bf872
Jihye <kimji9663@nate.com>
2021-12-10 17:36:15 +0900

    useEffect

--- react-hook/src/App.js
+++ react-hook/src/App.js
@@ -1,34 +1,19 @@
-import { useState } from 'react';
-
-const useTabs = (initialTabs, allTabs) => { // initial 초기값 = 0
-  const [currentIndex, setCurrentIndex] = useState(initialTabs);
-  if(!allTabs || !Array.isArray(allTabs)){
-    return;
-  }
-  return { currentItem: allTabs[currentIndex], changeItem: setCurrentIndex };
-};
-
-const content = [
-  {
-    tab: "Section 1",
-    content: "Section 1 콘텐츠임.."
-  },
-  {
-    tab: "Section 2",
-    content: "Section 2 콘텐츠임.."
-  }
-]
+import { useEffect, useState } from 'react';
 
 
 //App
 const App = () => {
-  const { currentItem, changeItem } = useTabs(0, content);
+  const [ number, setNumber ] = useState(0);
+  const [ anumber, setAnumber ] = useState(0);
+
+  const sayHello = () => console.log("hello");
+  useEffect(sayHello, []);
+
   return (
     <div className="App">
-      {content.map((cont, index)=>
-        <button type="button" onClick={() => changeItem(index)}>{cont.tab}</button>
-      )}
-      <div>{currentItem.content}</div>
+      <div>Hi</div>
+      <button type="button" onClick={() => setNumber(number + 1)}>{number}</button>
+      <button type="button" onClick={() => setAnumber(anumber + 1)}>{anumber}</button>
     </div>
   );
 }
