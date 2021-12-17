99b1699ad31acdd6cefdc4db53195f105a761bbf
Jihye <kimji9663@nate.com>
2021-12-17 14:36:16 +0900

    useBeforeLeave 다시풀기

--- react-hook/src/App.js
+++ react-hook/src/App.js
@@ -1,12 +1,13 @@
 //import UseConfirm from './component/UseConfirm';
-import UsePreventLeave from './component/UsePreventLeave';
+//import UsePreventLeave from './component/UsePreventLeave';
+import UseBeforeLeave from './component/UseBeforeLeave';
 //import UseEffect from './component/UseEffect';
 //import UseTitle from './component/UseTitle';
 
 //App
 const App = () => {
   return (
-    <UsePreventLeave></UsePreventLeave>
+    <UseBeforeLeave></UseBeforeLeave>
   );
 }
 
