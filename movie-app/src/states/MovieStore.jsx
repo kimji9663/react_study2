d11934e4b100ebdba435f3bdc4b0e4e71640ed32
kimji9663 <rlawlgp9663@naver.com>
2022-03-07 23:25:55 +0900

    mobx 예제

---
+++ movie-app/src/states/MovieStore.jsx
@@ -0,0 +1,26 @@
+import {action, makeObservable, observable} from "mobx";
+
+class Movie {
+    id;
+    title;
+    rate;
+
+    constructor(id, title, rate) {
+        this.id = id;
+        this.title = title;
+        this.rate = rate;
+    }
+}
+
+export class MovieStore {
+    rootStore;
+
+    movies = [];
+
+    constructor(root) {
+        makeObservable(this, {
+            movies: observable,
+        })
+        this.rootStore = root;
+    }
+}
