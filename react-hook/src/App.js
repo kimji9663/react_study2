a51cc9f16c1a2e79ed3d1ca55d9c53c152b7e1fe
kimji9663 <rlawlgp9663@naver.com>
2022-03-07 23:31:07 +0900

    merge

--- react-hook/src/App.js
+++ react-hook/src/App.js
@@ -1,33 +1,29 @@
 import { useState } from 'react';
 
-// useInput component
-const useInput = (initial, validator) => {
-  const [value, setValue] = useState(initial);
-  const onChange = (event) => {
-    const { target: {value} } = event;
-
-    let willUpdate = true;
-
-    if (typeof validator === "function"){
-      willUpdate = validator(value);
-    }
-
-    if (willUpdate){
-      setValue(value);
-    }
+const content = [
+  {
+    tab: 'Section1',
+    content: 'Section 1입니다.'
+  },
+  {
+    tab: 'Section2',
+    contents: 'Section 2입니다.'
   }
-  return {value, onChange};
+]
+
+const useTabs = (initialTab, allTabs) => {
+  
 }
 
 //App
 const App = () => {
-  const maxLength = (text) => text.length <= 10;
-  const name = useInput("Ms.", maxLength);
 
   return (
     <div className="App">
-      <input type="text" placeholder="mmm" value={name.value} onChange={name.onChange} />
-      {console.log(name)}
+      <h2>Hello</h2>
+      {content.map( section => (
+        <button type="button">{section.tab}</button>
+      ))}
     </div>
   );
 }
