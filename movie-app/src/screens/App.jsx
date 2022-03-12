52a2a92c0d63dc9782849975a5bbf0281184a462
kimji9663 <rlawlgp9663@naver.com>
2022-03-13 03:37:17 +0900

    mobx 예제

--- movie-app/src/screens/App.jsx
+++ movie-app/src/screens/App.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {Row, Col, Divider, Button, Modal, Input, Rate} from 'antd';
 import Card from '../components/Card';
 import { useStores } from '../states/Context';
+import {observer} from "mobx-react";
 
 // const initCache = [
 //     {
@@ -21,7 +22,7 @@ import { useStores } from '../states/Context';
 //     },
 // ]
 
-export default () => {
+export default observer(() => {
     const [isModalOpen, setIsModalOpen] = React.useState(false);
     const [newRate, setNewRate] = React.useState(0);
     const [newTitle, setNewTitle] = React.useState('');
@@ -33,13 +34,49 @@ export default () => {
     }
 
     const onModalOk = () => {
-        setCache([
-            ...cache,
-            {
-                id: cache[cache.length - 1].id,
-                title: newTitle,
-                rate: newRate,
-            }
-        ])
+        movieStore.createMovie(newTitle, newRate);
+        setNewRate(0);
+        setNewTitle('');
+        setIsModalOpen(false);
     }
-}
+
+    const onDelete = (id) => {
+        movieStore.deleteMovie(id);
+    }
+
+    const onExistingRateChange = (id, value) => {
+        movieStore.changeRate(id, value);
+    }
+
+    return (
+        <>
+            <Row justify="center">
+                <Button type="primary" danger onClick={() => setIsModalOpen(true)}>
+                    추가하기
+                </Button>
+            </Row>
+            <Divider/>
+            {
+                movieStore.movies.map(
+                    (x) => (
+                        <>
+                            <Row justify="center">
+                                <Card 
+                                    key={x.id}
+                                    title={x.title}
+                                    rate={x.rate}
+                                    onChange={(value) => onExistingRateChange(x.id, value)}
+                                    onDelete={() => onDelete(x.id)}
+                                />
+                            </Row>
+                        </>
+                    )
+                )
+            }
+            <Modal title="추가하기" visible={isModalOpen} onOk={onModalOk} onCancel={() => setIsModalOpen(false)}>
+                <Input placeholder="영화의 이름을 입력 해주세요." value={newTitle} onChange={(e) => setNewTitle(e.target.value)} />
+                <Rate onChange={onRateChange} value={newRate} />
+            </Modal>
+        </>
+    )
+})
--- movie-app/src/states/MovieStore.jsx
+++ movie-app/src/states/MovieStore.jsx
@@ -20,6 +20,9 @@ export class MovieStore {
     constructor(root) {
         makeObservable(this, {
             movies: observable,
+            createMovie: action,
+            deleteMovie: action,
+            changeRate: action,
         })
 
         this.rootStore = root;
@@ -28,6 +31,29 @@ export class MovieStore {
             new Movie(1, 'LOTR', 5),
             new Movie(2, 'Harry Potter', 4),
             new Movie(3, '창궐', 0),
+        ]
+    }
+
+    createMovie(title, rate){
+        this.movies = [
+            ...this.movies,
+            new Movie(this.movies[this.movies.length - 1].id + 1, title, rate),
+        ]
+    }
+
+    deleteMovie(id){
+        this.movies = this.movies.filter(x => x.id != id);
+    }
+
+    changeRate(id, rate){
+        const idx = this.movies.findIndex(x => x.id === id);
+        const movie = this.movies[idx];
+
+        // 리스트를 새로 뿌려준다.
+        this.movies = [
+            ...this.movies.slice(0, idx), // slice(0, 2) = 0, 1
+            new Movie(movie.id, movie.title, rate), // id, title은 그대로 rate는 바뀐 값 = 2
+            ...this.movies.slice(idx + 1, this.movies.length), //slice(3, 5) = 3, 4 (movis의 수가 5라고 가정)
         ]
     }
 }
