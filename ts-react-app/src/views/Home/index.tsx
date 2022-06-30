10ac33f500040076fb1273599e0ac453b7812be6
Jihye <kimji9663@nate.com>
2022-06-30 18:56:15 +0900

    스크롤 높이 지정, 현재 씬 활성화

--- ts-react-app/src/views/Home/index.tsx
+++ ts-react-app/src/views/Home/index.tsx
@@ -1,50 +1,129 @@
 
-import React from 'react'
-import { showScene0, showScene1, showScene2, showScene3, scrollSec0, scrollSec1, scrollSec2, scrollSec3, stickyElem, mainMessage } from './index.css';
+import React, { useState, useEffect } from 'react'
+import { showScene0, showScene1, showScene2, showScene3, scrollSec0, scrollSec1, scrollSec2, scrollSec3, stickyElem, mainMessage } from './index.css'
+import { debounce } from 'lodash'
+
+interface SceneInfo {
+  id: number
+  type: string
+  heightNum: number
+  scrollHeight: number
+  objs?: {
+    container: Element | null
+  }
+}
+
+const sceneInfo: SceneInfo[] = [
+  {
+    id: 0,
+    type: 'sticky',
+    heightNum: 5, // 스크롤 높이 = 화면 높이 * heightNum
+    scrollHeight: 0,
+    objs: {
+      container: document.querySelector('#scroll-section-0')
+    }
+  },
+  {
+    id: 1,
+    type: 'normal',
+    heightNum: 1,
+    scrollHeight: 0,
+    objs: {
+      container: document.querySelector('#scroll-section-1')
+    }
+  },
+  {
+    id: 2,
+    type: 'sticky',
+    heightNum: 5,
+    scrollHeight: 0,
+    objs: {
+      container: document.querySelector('#scroll-section-2')
+    }
+  },
+  {
+    id: 3,
+    type: 'normal',
+    heightNum: 5,
+    scrollHeight: 0,
+    objs: {
+      container: document.querySelector('#scroll-section-3')
+    }
+  },
+]
 
 function Home() {
+  let yOffset = 0 //window.pageYOffset 대신 쓸 변수(현재 위치값)
+  let prevScrollHeight = 0 // 현재 위치한 스크롤 위치값 이전의 섹션 높이값들의 합
+  let currentScene = 0 // 현재 보이는 scene(scroll-section)
+  let enterNewScene = false // 새 scene에 진입하면 true가 됨
 
-    let yOffset = 0; //window.pageYOffset 대신 쓸 변수(현재 위치값)
-    let prevScrollHeight = 0; // 현재 위치한 스크롤 위치값 이전의 섹션 높이값들의 합
-    let currentScene = 0; // 현재 보이는 scene(scroll-section)
-    let enterNewScene = false; // 새 scene에 진입하면 true가 됨
+  const [height, setHeight] = useState([
+    sceneInfo[0].scrollHeight,
+    sceneInfo[1].scrollHeight,
+    sceneInfo[2].scrollHeight,
+    sceneInfo[3].scrollHeight
+  ])
 
-    (() => {
-      const sceneInfo = [
-        {
-          // 0
-          type: 'sticky',
-          heightNum: 5, // 화면 높이 * heightNum
-          scrollHeight: 0,
-        },
-        {
-          // 1
-          type: 'normal',
-          heightNum: 5,
-          scrollHeight: 0,
-        },
-        {
-          // 2
-          type: 'sticky',
-          heightNum: 5,
-          scrollHeight: 0,
-        },
-        {
-          // 3
-          type: 'normal',
-          heightNum: 5,
-          scrollHeight: 0,
-        },
-      ];
+  function setLayout() {
+    // 각 스크롤 섹션의 높이 세팅
+    for (let i = 0; i < sceneInfo.length; i++) {
+      sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight
+      //sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`
+    }
+    //console.log(sceneInfo)
+  }
+  //window.addEventListener('resize', setLayout)
+  setLayout()
 
-      function setLayout() {
-        
-      }
-    });
+  function scrollLoop() {
+    prevScrollHeight = 0
+    for(let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight
+    }
+
+    if (yOffset > (prevScrollHeight + sceneInfo[currentScene].scrollHeight)) {
+      currentScene++
+    }
+
+    if (yOffset < prevScrollHeight) {
+      if (currentScene === 0) return
+      currentScene--
+    }
+
+    document.body.setAttribute('id', `show_scroll_section_${currentScene}`)
+
+    console.log(currentScene)
+  }
+
+  const handleResize = debounce(() => {  // 일정 시간이 지나고 함수가 한번만 실행됨
+    setLayout()
+    setHeight([
+      sceneInfo[0].scrollHeight,
+      sceneInfo[1].scrollHeight,
+      sceneInfo[2].scrollHeight,
+      sceneInfo[3].scrollHeight
+    ])
+  }, 100)
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return () => {
+      // 기존 리스너 제거
+      window.addEventListener('resize', handleResize)
+    }
+  }, []) // 빈 배열 전달하면 컴포넌트가 마운트, 언마운트 될 때만 실행
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      yOffset = window.pageYOffset
+      scrollLoop()
+    })
+  }, [])
 
   return (
     <>
-      <section className={scrollSec0.root} id="scroll_section_0">
+      <section className={scrollSec0.root} id="scroll_section_0" style={{ height: height[0] }}>
         <h1 className={scrollSec0.title}>AirMug Pro</h1>
         <div className={stickyElem}>
           움직이는 이미지
@@ -62,13 +141,13 @@ function Home() {
           <p className={mainMessage.text}>새롭게 입가를<br />찾아온 매혹</p>
         </div>
       </section>
-      <section className={scrollSec1.root} id="scroll_section_1">
+      <section className={scrollSec1.root} id="scroll_section_1" style={{ height: height[1] }}>
         <p className="description">
           <strong>보통 스크롤 영역</strong>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus exercitationem molestias voluptate repudiandae veniam aliquid rem nihil incidunt, sed esse cupiditate est, laborum officia voluptatum nemo. Amet quos aliquid dolor, perferendis dicta a, nostrum at numquam possimus minima repellendus pariatur veniam minus accusamus expedita distinctio sed dolore quibusdam? Facilis iure ratione sit voluptas laborum quisquam aliquam, quibusdam placeat accusantium dicta eos neque fuga pariatur blanditiis magnam dolore et expedita cumque. Quo error maiores excepturi perspiciatis, ullam dicta ad magni quia suscipit aliquam consequatur officia. Animi rerum tenetur ea aut ullam inventore, nisi deleniti! Modi laboriosam corrupti aperiam placeat veritatis totam animi vitae autem culpa expedita voluptate amet ad assumenda at enim harum nihil quasi nemo illo, repellat eveniet nam consectetur alias illum! Eius velit quod et possimus modi corporis cupiditate deleniti, consequatur sit, blanditiis officia minima dignissimos tempora hic necessitatibus inventore natus culpa laboriosam tenetur mollitia aliquid. Earum obcaecati a similique reiciendis dolore aliquid pariatur reprehenderit voluptates, qui nemo non eius, culpa libero tempore deleniti mollitia laboriosam consequuntur dolorem perspiciatis facere cum excepturi error praesentium illo? Itaque quo ea dignissimos molestias eius molestiae, tempore quod dolor, placeat corrupti magnam dolores saepe omnis praesentium dolore voluptate recusandae tempora aspernatur ut autem!
         </p>
       </section>
-      <section className={scrollSec2.root} id="scroll_section_2">
+      <section className={scrollSec2.root} id="scroll_section_2" style={{ height: height[2] }}>
         <div className={stickyElem}>
           움직이는 이미지
         </div>
@@ -88,7 +167,7 @@ function Home() {
           <div className="pin"></div>
         </div>
       </section>
-      <section className={scrollSec3.root} id="scroll_section_3">
+      <section className={scrollSec3.root} id="scroll_section_3" style={{ height: height[3] }}>
         <p className="mid_message">
           <strong>Retina 머그</strong>
           맛의 향연을 광활하게 펼칠<br />
@@ -99,7 +178,7 @@ function Home() {
         </p>
       </section>
     </>
-  );
+  )
 }
 
 export default Home
